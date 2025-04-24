e5c79a3feb995c5c83eaa804143fd9fd557aed39
Sajid Patel <102913715+Sajid-Patel@users.noreply.github.com>
2025-04-24 15:43:35 +0530

    Add files via upload

---
+++ DSA_JAVA/Node.java
@@ -0,0 +1,15 @@
+package com.queue;
+
+//QueueDemo1 through Singly ArrayList
+
+//insert from last 
+// delete from first
+public class Node {
+	public Object data;
+	public Node previous;
+
+	public Node(int data) {
+		this.data = data;
+		this.previous = null;
+	}
+}
